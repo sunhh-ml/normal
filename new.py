112d57ec307200eb7947f4b5420d91affefb9441
sunhh-ml <sun58454006@163.com>
2020-04-10 14:37:39 +0800

    test add new py file

--- new.py
+++ new.py
@@ -1,10 +1,10 @@
 # !/usr/bin/env python3
 # -*- coding:utf-8 -*-
-# @Time         : 2020/4/10 14:15
+# @Time         : 2020/4/10 14:36
 # @Author       : Huanhuan sun
 # @Email        : sun58454006@163.com
 # @File         : new.py
 # @Software     : PyCharm
 # @Project      : normal
 
-print('im a new file')
+print("i'm a new file")
