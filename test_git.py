ff2bad99f6dc1332735fdf54e07f8f4d992e0493
sunhh-ml <sun58454006@163.com>
2020-04-10 16:09:12 +0800

    test add new py file

---
+++ test_git.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Time         : 2020/4/10 16:08
+# @Author       : Huanhuan sun
+# @Email        : sun58454006@163.com
+# @File         : test_git.py
+# @Software     : PyCharm
+# @Project      : normal
+
+print('这个用来测试新建的py文件，能否上传到规定git仓库中')
